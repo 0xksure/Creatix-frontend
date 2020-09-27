4c67d6db9ffc9946c5fe8ade06e8547233f47e3d
kristohberg <kristoffer.hovland.berg@gmail.com>
2020-09-27 19:41:34 +0200

    Add procfile and express server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.use('*', express.static(path.join(__dirname, '../dist')));
+
+app.listen(port);
