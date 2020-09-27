15ad753fad9c0ba29b00cc0d26db5e60b86c4246
kristohberg <kristoffer.hovland.berg@gmail.com>
2020-09-27 06:27:21 +0200

    Heroku: update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,36 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
-const path = require("path");
-const dotenv = require("dotenv");
-const ManifestPlugin = require("webpack-manifest-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
-const SitemapWebpackPlugin = require("sitemap-webpack-plugin").default;
-const CopyPlugin = require("copy-webpack-plugin");
+const SitemapWebpackPlugin = require('sitemap-webpack-plugin').default;
+const CopyPlugin = require('copy-webpack-plugin');
 
-const paths = ["/", "/discover"];
+const paths = ['/', '/discover'];
 
 module.exports = () => {
   return {
-    entry: { babel: "@babel/polyfill", app: "./src/index" },
+    entry: { babel: '@babel/polyfill', app: './src/index' },
     module: {
       rules: [
         {
           test: /\.(js|jsx|ts|tsx)$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
           },
         },
         {
           test: /\.s[ac]ss$/i,
           use: [
             {
-              loader: "style-loader", // creates style nodes from JS strings
+              loader: 'style-loader', // creates style nodes from JS strings
             },
             {
-              loader: "css-loader", // translates CSS into CommonJS
+              loader: 'css-loader', // translates CSS into CommonJS
             },
             {
-              loader: "sass-loader", // compiles Sass to CSS
+              loader: 'sass-loader', // compiles Sass to CSS
               options: {
                 sourceMap: true,
               },
@@ -42,23 +41,23 @@ module.exports = () => {
           test: /\.html$/,
           use: [
             {
-              loader: "html-loader",
+              loader: 'html-loader',
             },
           ],
         },
         {
           test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: "file-loader",
+          loader: 'file-loader',
         },
         {
           test: /\.svg$/,
           exclude: /node_modules/,
           use: [
             {
-              loader: "babel-loader",
+              loader: 'babel-loader',
             },
             {
-              loader: "react-svg-loader",
+              loader: 'react-svg-loader',
               options: {
                 jsx: true, // true outputs JSX tags
               },
@@ -68,58 +67,58 @@ module.exports = () => {
         {
           test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
           exclude: /node_modules/,
-          use: ["file-loader?name=[name].[ext]"],
+          use: ['file-loader?name=[name].[ext]'],
         },
         {
           test: /\.json$/,
-          loader: "json-loader",
+          loader: 'json-loader',
         },
       ],
     },
     resolve: {
-      modules: ["node_modules", "src"],
-      mainFiles: ["index"],
-      extensions: [".js", ".jsx", ".tsx", ".ts", ".json", ".scss", ".css"],
+      modules: ['node_modules', 'src'],
+      mainFiles: ['index'],
+      extensions: ['.js', '.jsx', '.tsx', '.ts', '.json', '.scss', '.css'],
       symlinks: false,
     },
     output: {
-      path: path.resolve(__dirname, "dist"),
-      publicPath: "/",
-      filename: "bundle.js",
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+      filename: 'bundle.js',
     },
     plugins: [
       new HtmlWebPackPlugin({
-        template: "./src/index.html",
-        filename: "./index.html",
+        template: './src/index.html',
+        filename: './index.html',
       }),
       new ManifestPlugin({
-        filename: "asset-manifest.json",
+        filename: 'asset-manifest.json',
       }),
       new CopyPlugin({
         patterns: [
-          { from: "src/Assets/images", to: "Assets/images" },
-          { from: "src/robots.txt", to: "robots.txt" },
+          { from: 'src/Assets/images', to: 'Assets/images' },
+          { from: 'src/robots.txt', to: 'robots.txt' },
         ],
       }),
       new webpack.HotModuleReplacementPlugin(),
       new webpack.EnvironmentPlugin({
-        TRACKING_ID: "",
-        GTM_ID: "",
-        NODE_ENV: "development",
-        apiKey: "",
-        API_URL: "http://localhost:8080/v0/",
-        authDomain: "",
-        databaseURL: "",
-        projectId: "",
-        storageBucket: "",
-        messagingSenderId: "",
-        appId: "",
-        measurementId: "",
+        TRACKING_ID: '',
+        GTM_ID: '',
+        NODE_ENV: 'development',
+        apiKey: '',
+        API_URL: 'http://localhost:8080/v0/',
+        authDomain: '',
+        databaseURL: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: '',
+        appId: '',
+        measurementId: '',
       }),
-      new SitemapWebpackPlugin("https://www.thecreatix.io", paths),
+      new SitemapWebpackPlugin('https://www.thecreatix.io', paths),
     ],
     devServer: {
-      contentBase: "./dist",
+      contentBase: './dist',
       hot: true,
       historyApiFallback: true,
     },
