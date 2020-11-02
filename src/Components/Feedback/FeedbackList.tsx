1da7ba32ddbee4ebcc90ce9e8d7e9e3d173f4c71
kristohberg <kristoffer.hovland.berg@gmail.com>
2020-11-02 19:28:04 +0100

    Fix feedback list size

--- src/Components/Feedback/FeedbackList.tsx
+++ src/Components/Feedback/FeedbackList.tsx
@@ -10,13 +10,11 @@ const FeedbackList: React.FC<Props> = ({ feedbacks }) => {
   console.log('feedback ', feedbacks);
   return (
     <div className="cell margin-top-l">
-      <div className="grid-x align-center">
-        <ul className="grid-x feedback-list ">
-          {feedbacks &&
-            feedbacks.length > 0 &&
-            feedbacks.map((feedback) => <FeedbackItem key={feedback.id} feedback={feedback} />)}
-        </ul>
-      </div>
+      <ul className="grid-x feedback-list align-center">
+        {feedbacks &&
+          feedbacks.length > 0 &&
+          feedbacks.map((feedback) => <FeedbackItem key={feedback.id} feedback={feedback} />)}
+      </ul>
     </div>
   );
 };
