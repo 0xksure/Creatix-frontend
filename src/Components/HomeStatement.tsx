ce0506bdc990c6699814ee56a1ddcbec47d3d162
kristohberg <kristoffer.hovland.berg@gmail.com>
2020-11-25 22:14:50 +0100

    CF-1 Discover needs an overhaul + home and pricing #1

--- src/Components/HomeStatement.tsx
+++ src/Components/HomeStatement.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React from 'react';
 
-const STATEMENT = "Business Transparency Made Simple";
-const SUB_STATEMENT =
-  "Creatix is about transparency. Transparency in what people want, how people work and how people evolve. It is about having the opportunity to be heard and ultimately improving the business together.";
+const TEXT = {
+  STATEMENT: 'Aggregated Feedback',
+  SUB_STATEMENT: (
+    <>
+      Creatix lets you collect, submit and aggregate feedback in order for you to make the best
+      decisions possible. By creating new feedback on the fly you can focus on the next task and
+      analyse the data <span className="p--pink">when</span> you want. You can subscribe to feedback
+      channels of your choice. With the help of text recognizion we are able to tag your feedback
+      into useful categories and find out which feedbacks are most similar. Thus helping you figure
+      out which feedback need the most attention.
+    </>
+  ),
+};
 
 const HomeStatement: React.FC = () => {
   return (
@@ -10,10 +20,10 @@ const HomeStatement: React.FC = () => {
       <div className="cell small-12 home-statement">
         <div className="text-content">
           <h1 className="h1 large" id="home_statement_main">
-            {STATEMENT}
+            {TEXT.STATEMENT}
           </h1>
           <p className="p large" id="home_sub_statement_main">
-            {SUB_STATEMENT}
+            {TEXT.SUB_STATEMENT}
           </p>
         </div>
       </div>
